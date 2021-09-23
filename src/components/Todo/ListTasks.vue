cc7261bc9ea4d9c9114109176a344ef1b31bab95
Jim <jim.h@shopperplus.com>
2021-09-23 16:44:54 +0800

    refactoring template into child components.

---
+++ src/components/Todo/ListTasks.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-list
+    subheader
+    two-line
+    flat
+  >
+    <task
+      v-for="task in $store.state.tasks"
+      :key="task.id"
+      :task="task"
+    />
+  </v-list>
+</template>
+
+<script>
+	export default {
+    components: {
+      'task': require('@/components/Todo/Task').default,
+    }
+	}
+</script>
